d68eb6caf454d7d3403ae8002a55e265ef76c43c
Hokague <criativasayte@gmail.com>
2024-05-22 11:47:00 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
